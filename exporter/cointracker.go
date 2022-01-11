a5553630c22aa196adba9e9e33e8553cc0d8cac6
m4dc0w <m4dc0w@users.noreply.github.com>
2022-01-10 20:40:07 -0800

    Add support for ASA transfers.

--- exporter/cointracker.go
+++ exporter/cointracker.go
@@ -27,10 +27,17 @@ func (k *cointrackerExporter) WriteHeader(writer io.Writer) {
 func (k *cointrackerExporter) WriteRecord(writer io.Writer, record ExportRecord) {
 	//Date,Received Quantity,Received Currency,Sent Quantity,Sent Currency,Fee Amount,Fee Currency,Tag
 	fmt.Fprint(writer, record.blockTime.UTC().Format("01/02/2006 15:04:05,"))
-	fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.recvQty))
-	fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.sentQty))
+	
+	if record.assetID != 0 {
+		// Custom decimal formatting is needed for certain ASAs.
+		fmt.Fprintf(writer, "%s,ASA-%d,", algoFmt(record.recvQty),record.assetID)
+		fmt.Fprintf(writer, "%s,ASA-%d,", algoFmt(record.sentQty),record.assetID)
+	} else {
+		fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.recvQty))
+		fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.sentQty))
+	}
 
-	if record.fee > 0 {
+	if record.fee != 0 {
 		fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.fee))
 	} else {
 		fmt.Fprintf(writer, ",,")
