b1d7520524150c699324067741b756df45ad9909
m4dc0w <m4dc0w@users.noreply.github.com>
2022-01-10 22:52:50 -0800

    Add ASA transaction output.

--- exporter/koinly.go
+++ exporter/koinly.go
@@ -3,6 +3,8 @@ package exporter
 import (
 	"fmt"
 	"io"
+
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 )
 
 func init() {
@@ -24,13 +26,12 @@ func (k *koinlyExporter) WriteHeader(writer io.Writer) {
 	fmt.Fprintln(writer, "Date,Sent Amount,Sent Currency,Received Amount,Received Currency,Fee Amount,Fee Currency,Net Worth Amount,Net Worth Currency,Label,Description,TxHash")
 }
 
-func (k *koinlyExporter) WriteRecord(writer io.Writer, record ExportRecord) {
+func (k *koinlyExporter) WriteRecord(writer io.Writer, record ExportRecord, assetMap map[uint64]models.Asset) {
 	//Date,Sent Amount,Sent Currency,Received Amount,Received Currency,Fee Amount,Fee Currency,Net Worth Amount,Net Worth Currency,Label,Description,TxHash
 	fmt.Fprintf(writer, "%s UTC,", record.blockTime.UTC().Format("2006-01-02 15:04:05"))
 	switch {
-	case record.sentQty != 0 && record.assetId != 0:
-		// Custom decimal formatting is needed for certain ASAs.
-		fmt.Fprintf(writer, "%s,ASA-%d,", algoFmt(record.sentQty),record.assetID)
+	case record.sentQty != 0 && record.assetID != 0:
+		fmt.Fprintf(writer, "%s,ASA-%d,", assetIDFmt(record.sentQty, record.assetID, assetMap),record.assetID)
 	case record.sentQty != 0:
 		fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.sentQty))
 	default:
@@ -38,9 +39,8 @@ func (k *koinlyExporter) WriteRecord(writer io.Writer, record ExportRecord) {
 	}
 
 	switch {
-	case record.recvQty != 0 && record.assetId != 0:
-		// Custom decimal formatting is needed for certain ASAs.
-		fmt.Fprintf(writer, "%s,ASA-%d,", algoFmt(record.recvQty),record.assetID)
+	case record.recvQty != 0 && record.assetID != 0:
+		fmt.Fprintf(writer, "%s,ASA-%d,", assetIDFmt(record.recvQty, record.assetID, assetMap),record.assetID)
 	case record.recvQty != 0:
 		fmt.Fprintf(writer, "%s,ALGO,", algoFmt(record.recvQty))
 	default:
